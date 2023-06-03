6dc8607bb649532fb50a7a7789cdac073fdc2e42
johndward01 <johndward01@gmail.com>
2023-06-02 21:39:09 -0500

    update

--- Classes_Exercise_01/Program.cs
+++ Classes_Exercise_01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+#region// TODO: Create a public Product class
+// TODO: Create a public Product class
+
+// TODO: Create a public ProductID property of type long
+// TODO: Create a public Name property of type string
+// TODO: Create a public Price property of type double
+// TODO: Create a public CategoryID property of type int
+// TODO: Create a public OnSale property of type bool
+// TODO: Create a public StockLevel property of type int
+
+// TODO: Create a default constructor 
+// TODO: Create a parameterized constructor that initializes all of the properties
+
+// TODO: Create a method that displays all of the properties creatively
+#endregion
+
